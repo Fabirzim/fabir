910f4732a35efd520d3619a81887d49a78092839
Fabiano Amaral <fabianoamaral47@hotmail.com>
2023-01-02 12:27:48 -0300

    fabir3

--- script.js
+++ script.js
@@ -1,16 +1,10 @@
+let nome="fabir"
 
-onload=function(){
-  let nome=localStorage.getItem('nome')
-  let h1=document.getElementById('nome')
-  h1.innerHTML=nome
-}
-
-
-function mudar(element){
-  let cor=element.value
-  let h1=document.getElementById('nome')
-  h1.innerHTML=cor
-
-
-  localStorage.setItem('nome',cor)
+try{
+    if(nome == ""){
+        throw "errado"
+    }
+    console.log(nome)
+}catch(err){
+    console.log('errado',err)
 }
