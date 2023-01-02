ee1855ac5c24fd3aaf19605af72f9eeeb8207e94
Fabiano Amaral <fabianoamaral47@hotmail.com>
2023-01-02 14:34:17 -0300

    fabir4

--- script.js
+++ script.js
@@ -1,10 +1,26 @@
-let nome="fabir"
+var nota1=9
+var nota2=4
+var media=(nota1+nota2)/2
+var conceito=""
+if(media>9){
+    conceito="otimo"
+}else if(media>3){
+    conceito="bom"
+}else{
+    conceito="ruim"
+}
 
-try{
-    if(nome == ""){
-        throw "errado"
-    }
-    console.log(nome)
-}catch(err){
-    console.log('errado',err)
+console.log(media)
+console.log(conceito)
+
+switch(conceito){
+    case "otimo":
+        console.log('esta bem')
+        break;
+        case "bom":
+            console.log('esta')
+            break;
+            case "ruim":
+                console.log('estta mal')
+                break;
 }
